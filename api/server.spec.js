b142f0cc2422eae9da54cf44794cc13f8e54c5e5
Jennifer King <jenbellpgh@gmail.com>
2019-02-13 00:30:24 -0500

    Easy (get) tests written. Broke code to check & fixed.

--- api/server.spec.js
+++ api/server.spec.js
@@ -18,5 +18,19 @@ describe('the /note routes', () => {
     expect(response.status).toEqual(200);
   });
 
-  test('/note/get/all returns an object list', () => {});
+  test('/note/get/all returns an object list', async () => {
+    const response = await request(server).get('/note/get/all');
+
+    expect(response.status).toEqual(200);
+    expect(response.type).toBe('application/json');
+    expect(response.body).not.toEqual({ error: "Note information could not be retrieved." });
+  });
+
+  test('/note/get/id returns a note', async () => {
+    const response = await request(server).get('/note/get/1');
+
+    expect(response.status).toEqual(200);
+    expect(response.type).toBe('application/json');
+    expect(response.body).not.toEqual({ error: `Note 1 could not be found.` });
+  });
 });
